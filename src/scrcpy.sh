52dde618d7937245379959c39e3e1b103ade82e6
Gregor Richner <gregor.richner-extern@deutschebahn.com>
2022-11-30 18:05:21 +0100

    revert

--- src/scrcpy.sh
+++ src/scrcpy.sh
@@ -2,8 +2,7 @@
 
 if [ -z "$REAL_DEVICE"]; then
   echo "Container is using android emulator"
-  sleep 1
 else
   echo "Starting android screen copy..."
-  /usr/bin/scrcpy
+  /usr/local/bin/scrcpy
 fi
--- src/vnc.sh
+++ src/vnc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-vnc="/usr/bin/x11vnc -display ${DISPLAY} -forever -shared -nonc "
+vnc="/usr/bin/x11vnc -display ${DISPLAY} -forever -shared "
 
 pass_path="/root/vncpass"
 if [ ! -z "${VNC_PASSWORD}" ]; then
